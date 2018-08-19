8006caee21d1efa77a770ae2b6ef6238100c816d
ashique-ubuntu <ashique12009@gmail.com>
2018-08-19 16:53:19 +0600

    Information is saving properly

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,35 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-Vue.component('example', require('./components/Example.vue'));
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+let Myheader = require('./components/Myheader.vue');
+let Myfooter = require('./components/Myfooter.vue');
+let Home = require('./components/Home.vue');
+let About = require('./components/About.vue');
+
+const routes = [
+  { path: '/home', component: Home },
+  { path: '/about', component: About }
+];
+
+const router = new VueRouter({
+    routes
+});
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+
+    created(){
+        console.log('Hello world');
+    },
+
+    router,
+
+    components:{
+        Myheader,
+        Myfooter
+    }
 });
