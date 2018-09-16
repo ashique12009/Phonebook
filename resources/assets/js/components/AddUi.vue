2c80f4d2ee225dcded7beee402885df480445333
ashique <ashique12009@gmail.com>
2018-09-16 11:35:09 +0600

    Popup update

--- resources/assets/js/components/AddUi.vue
+++ resources/assets/js/components/AddUi.vue
@@ -4,42 +4,40 @@
         <!-- Modal -->
         <div id="myModal" class="modal fade" role="dialog">
             <div class="modal-dialog">
-
                 <!-- Modal content-->
                 <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Add Information</h4>
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">Add Information</h4>
+                    </div>
+                    <div class="modal-body">
+                        <div class="alert alert-danger" v-if="errors.name">
+                            {{ errors.name[0] }}
+                        </div>
+                        <div class="alert alert-danger" v-if="errors.phone">
+                            {{ errors.phone[0] }}
+                        </div>
+                        <div class="alert alert-danger" v-if="errors.email">
+                            {{ errors.email[0] }}
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Name</label>
+                            <input class="form-control" value="" type="text" v-model="list.name">
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Phone</label>
+                            <input class="form-control" value="" type="text" v-model="list.phone">
+                        </div>
+                        <div class="form-group">
+                            <label class="control-label">Email</label>
+                            <input class="form-control" value="" type="email" v-model="list.email">
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary" @click="save">Submit</button>
+                    </div>
                 </div>
-                <div class="modal-body">
-                    <div class="alert alert-danger" v-if="errors.name">
-                        {{ errors.name[0] }}
-                    </div>
-                    <div class="alert alert-danger" v-if="errors.phone">
-                        {{ errors.phone[0] }}
-                    </div>
-                    <div class="alert alert-danger" v-if="errors.email">
-                        {{ errors.email[0] }}
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label">Name</label>
-                        <input class="form-control" value="" type="text" v-model="list.name">
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label">Phone</label>
-                        <input class="form-control" value="" type="text" v-model="list.phone">
-                    </div>
-                    <div class="form-group">
-                        <label class="control-label">Email</label>
-                        <input class="form-control" value="" type="email" v-model="list.email">
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary" @click="save">Submit</button>
-                </div>
-                </div>
-
             </div>
         </div>
     </span>
